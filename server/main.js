a4b7af986c01654728656696de129d81bc3f9a96
Sergey Poznyak <Sergeeeekg@gmail.com>
2018-10-26 15:10:45 +0300

    Added basic test case

--- server/main.js
+++ server/main.js
@@ -1,5 +1,35 @@
 import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  const ParentCollection = new Mongo.Collection('parent');
+  const ChildCollection = new Mongo.Collection('child');
+
+  ParentCollection.addLinks({
+    childrenLink: {
+      type: 'many',
+      collection: ChildCollection,
+      field: 'childrenIds',
+    },
+  })
+
+  if (ChildCollection.find().count() === 0) {
+    for (let i = 0; i < 100; i++) {
+      ChildCollection.insert({ _id: `test${i}` });
+    }
+    ParentCollection.insert({
+      _id: 'parent1',
+      childrenIds: new Array(100).fill().map((_, idx) => `test${idx}`),
+    });
+  }
+
+  const queryResult = ParentCollection.createQuery({
+    childrenLink: {
+      _id: 'test50',
+    },
+  }).fetchOne();
+
+  if (queryResult.childrenLink.length !== 1) {
+    throw new Meteor.Error(`_id filters for links don\'t work. Returned count is ${queryResult.childrenLink.length}, while it should be 1`);
+  }
 });
